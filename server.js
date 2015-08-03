7cd00908396b82808fbb26df04ccbada6a16bead
armen.tarlamazyan <javascriptronin@gmail.com>
2015-08-03 16:44:17 +0300

    ready to deploy

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var stylus = require('stylus');
 var conf = require('./server/config');
 var morgan  = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-var port = conf.get('port');
+var port = process.env.PORT || conf.get('port');
+var db = mongoose.connection;
 
 function compite(str, path) {
     return stylus(str).set('filename', path);
@@ -24,12 +26,39 @@ app.use(stylus.middleware({
 }));
 app.use(express.static(__dirname + '/public'));
 
+
+if (env === 'development') {
+    mongoose.connect('mongodb://localhost/blog');
+} else {
+    mongoose.connect('mongodb://DeWitt:IsaacClark@ds045089.mongolab.com:45089/blog');
+}
+
+db.on('error', console.error.bind(console, 'connection error...'));
+
+db.once('open', function callback() {
+    console.log('blog db opened.');
+});
+
+var messageSchema = mongoose.Schema({
+    message: String
+});
+
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+
+Message.findOne().exec(function (err, messageDoc) {
+    mongoMessage = messageDoc.message
+});
+
+
 app.get('/partials/:partialPath', function (req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function (req, res) {
-    res.render('index')
+    res.render('index', {
+        mongoMessage: mongoMessage
+    })
 });
 
 app.listen(port);
